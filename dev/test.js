9edcf849b589ef603935ae56f0f47c59390fc8cd
Kindstory <sjwgkdl@naver.com>
2023-02-15 23:17:41 +0900

    BlockChain_JS

---
+++ dev/test.js
@@ -0,0 +1,36 @@
+const Blockchain = require('./blockchain');
+
+const bitcoin = new Blockchain();
+
+const previousBlockHash = '88870sddsfgdgsdfgfdsg22';
+
+const currentBlockData = [
+    {
+        amount:10,
+        sender:'G4353453',
+        recipient:'3SDFASD3',
+    }
+]
+
+const nonce = 100;
+
+bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce);
+
+
+console.log(bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce));
+
+// bitcoin.createNewBlock(2333, 'ASDFDSAADS', '789sd090df');
+
+// bitcoin.createNewTransaction(100, 'ASDFGG345345', 'JDE32523f');
+
+// bitcoin.createNewBlock(756756, 'SGGGHHSDDD', 'WEF23423432WDSF');
+
+// bitcoin.createNewTransaction(50, 'SDFDSGDSGDSAGSD', 'GSAG23432SDF');
+// bitcoin.createNewTransaction(300, 'SDFDSGDSGDSAGSD', 'GSAG23432SDF');
+// bitcoin.createNewTransaction(2500, 'SDFDSGDSGDSAGSD', 'GSAG23432SDF');
+
+// bitcoin.createNewBlock(0909808, 'ASDFSFASDF', 'DSFA4352');
+
+
+
+// console.log(bitcoin);
